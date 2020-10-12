a07d53baf8e8bec5023e805fb5614e00c80e394c
Prem Bahadur Chand <pcmsia@gmail.com>
2020-10-12 19:02:26 +0200

    run deploy properties in the terminal

---
+++ build/precache-manifest.3a6e3aaa0cb08c32fe60095b53207425.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c81436de44b0c97747e17949adc545a",
+    "url": "/github-user-search-app/index.html"
+  },
+  {
+    "revision": "91c419b90c497071a243",
+    "url": "/github-user-search-app/static/css/main.6845de5b.chunk.css"
+  },
+  {
+    "revision": "1460984f42cdae978e87",
+    "url": "/github-user-search-app/static/js/2.c5f5847e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-user-search-app/static/js/2.c5f5847e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91c419b90c497071a243",
+    "url": "/github-user-search-app/static/js/main.ebdcaf5d.chunk.js"
+  },
+  {
+    "revision": "69179e152ae62bf46923",
+    "url": "/github-user-search-app/static/js/runtime-main.2f4fbbca.js"
+  }
+]);
