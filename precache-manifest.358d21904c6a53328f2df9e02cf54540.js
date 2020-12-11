e2d754a017675b36f4eb6e2c94abd23afef11721
Prem Bahadur Chand <pcmsia@gmail.com>
2020-12-11 15:46:15 +0100

    Updates

---
+++ precache-manifest.358d21904c6a53328f2df9e02cf54540.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f3a62833be120b16bf7d6b6e47e5f84",
+    "url": "/github-user-search-app/index.html"
+  },
+  {
+    "revision": "cd78079db77965a8e0da",
+    "url": "/github-user-search-app/static/css/main.6845de5b.chunk.css"
+  },
+  {
+    "revision": "ccb63e403721c03dfc2c",
+    "url": "/github-user-search-app/static/js/2.15cc215b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-user-search-app/static/js/2.15cc215b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd78079db77965a8e0da",
+    "url": "/github-user-search-app/static/js/main.07bf0316.chunk.js"
+  },
+  {
+    "revision": "8520f79a99c836c35cfd",
+    "url": "/github-user-search-app/static/js/runtime-main.0e6a399f.js"
+  }
+]);
